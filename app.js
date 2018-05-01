e56c86c696f8c11b23ca58df7ac32f6657b19c06
Eddie Yepez <eddieyepez@yahoo.com>
2018-05-01 15:13:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,149 +1,214 @@
-$(document).ready(function($){
+"use strict";
+let results = [];
+let imagesArr = [];
+let searchFor = "";
+let searchTerm;
 
-	var results = [];
-	var counter = 0;
-	
-	$( "#searchClick" ).on( "click", function(e){
+window.onload = function() {
+
+	let searchBtn = document.querySelector('#searchClick');
+	searchBtn.addEventListener("click", (e) => {
 		e.preventDefault();
 		results = [];
-		counter = 0;
-		$("#results").empty();
-		searchWiki($('#searchField').val());
-	});
+		imagesArr = [];
+		searchFor = document.querySelector('#searchField').value;
 
-
-	function searchWiki(searchFor){	
-
-		//console.log(results);
-
-		$.ajax({
-			url: '//en.wikipedia.org/w/api.php',
-		  	data: {
-			    action: 'query',
-			    list: 'search',
-			    srsearch: searchFor,
-			    format: 'json',
-			    formatversion: 2
-		  	},
-		  	dataType: 'jsonp',
-		  	type: 'POST',
-		    headers: { 'Api-User-Agent': 'eyMedia/1.0' },
-		  	success: function (x) {  		
-
-		  		for(var i =0; i<x.query.search.length; i++){
-
-		  			results.push({
-		  				'id': i,
-		  				'pageid' : x.query.search[i].pageid,
-		  				'title' : x.query.search[i].title,
-		  				'snippet' : x.query.search[i].snippet,
-		  				'pageURL' : 'https://en.wikipedia.org/?curid='+ x.query.search[i].pageid
-		  			});
-
-		  			getImages(i);
-		  			//getThumb(i);
-		  		}
-
-		  		console.log(results);
-		  		//populate();
-		  	}
-
-		});		
-
-	}
-
-	function getImages(index){		
-
-		$.ajax({
-
-  			type: "GET",
-	        url: "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=images&titles="+results[index].title+"&format=json&callback=?",
-	        contentType: "application/json; charset=utf-8",
-	        async: false,
-	        dataType: "json",
-	        success: function (data, textStatus, jqXHR) {
-	        	results[index].imageName = data.query.pages[results[index].pageid].images[0].title.substring(5);
-	        	getImageURL(index)
-	        },
-
-  		});
-	}
-
-	function getImageURL(index){		
-
-		$.ajax({
-
-  			type: "GET",
-	        url: "https://en.wikipedia.org/w/api.php?action=query&format=json&titles=Image:"+results[index].imageName+"&prop=imageinfo&iiprop=url&callback=?",
-	        contentType: "application/json; charset=utf-8",
-	        async: false,
-	        dataType: "json",
-	        success: function (data, textStatus, jqXHR) {
-	        	results[index].imageURL = data.query.pages[Object.keys(data.query.pages)[0]].imageinfo[0].url;
-	        	getThumb(index);
-	        },
-
-  		});
-	}
-
-	function getThumb(index){		
-
-		$.ajax({
-
-  			type: "GET",
-	        url: "https://en.wikipedia.org/w/api.php?action=query&titles="+results[index].title+"&prop=pageimages&format=json&pithumbsize=200&callback=?",
-	        contentType: "application/json; charset=utf-8",
-	        async: false,
-	        dataType: "json",
-	        success: function (data, textStatus, jqXHR) {
-	        	if(data.query.pages[results[index].pageid].thumbnail){
-	        		results[index].imageThumb = data.query.pages[results[index].pageid].thumbnail.source;
-	        	}else{
-	        		results[index].imageThumb = '';
-	        	}
-	        	counter++;
-
-	        },
-
-  		});
-
-	}
-
-	$( document ).ajaxComplete(function() {
-		if(counter === 10 ){
-			populate();
+		if(searchFor !== ""){
+			searchTerm = document.querySelector('#searchField').value;
+			search(searchFor);
+		}else{
+			randomSearch();
 		}
-		
+
 	});
+}
 
-	function populate(){
+let randomSearch = () => {
 
-		for(var i =0; i<results.length; i++){
+	let url = "https://en.wikipedia.org//w/api.php?action=query&format=json&list=random&rnlimit=1&origin=*";
 
-			var imageData = "";
+	fetch(url).then((res) => {
+		res.json().then((data) => {
+			let searchFor = data.query.random[0].title;
+			let n = searchFor.indexOf(":");
 
-			if(results[i].imageThumb !== ""){
-				imageData = "<img class='card-img-top' src='"+results[i].imageThumb+"' alt='Card image cap'>";
-			}else if (results[i].imageURL !== "" ){
-				imageData = "<img class='card-img-top' src='"+results[i].imageURL+"' alt='Card image cap'>";
+			if(n != -1){
+				return randomSearch();
 			}else{
-				imageData = "<img class='card-img-top' src='https://en.wikipedia.org/wiki/Wikipedia#/media/File:Wikipedia-logo-v2.svg' alt='Card image cap'>";
+				searchTerm = searchFor;
+				return search(searchFor);
 			}
+
+		});
+	});
+}
+
+let search = (searchFor) => {
+
+	let url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch="+searchFor+"&origin=*";
+
+	fetch(url).then((res) => {
+		res.json().then((data) => {
+			firstQuery(data);
+			return getImages(data);
+		});
+	});
+}
+
+let firstQuery = data => {
+
+	data.query.search.map((value, index) => {
+		results.push({
+			'id': index,
+			'pageid' : value.pageid,
+			'title' : value.title,
+			'snippet' : value.snippet,
+			'pageURL' : 'https://en.wikipedia.org/?curid='+ value.pageid
+		});
+	})
+}
+
+let getImages = (data) =>{
+
+	let urls = [];
+
+	for(let i = 0; i < data.query.search.length; i++){
+
+		let title = data.query.search[i].title;
+		let urlConstructor = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=images&titles="+title+"&format=json&origin=*";
+		urls.push(urlConstructor);
+	}
+
+	Promise.all(urls.map(url =>
+	    fetch(url).then(resp => resp.json())
+	)).then(imageData => {
+
+	    imageData.map((info, i) =>{   	    
+	
+			let mainImage;			
+			let query = info.query.pages[data.query.search[i].pageid].images;
+
+			for (var key in query){
+
+				if( query[key].title !== 'File:Disambig gray.svg'){
+
+					if( query[key].title !== 'File:DAB list gray.svg'){
+
+						if( query[key].title !== 'File:Ambox important.svg'){
+
+							if( query[key].title !== 'File:Commons-logo.svg'){
+
+								let type = query[key].title.substr(query[key].title.length - 3);
+
+								if( type === 'jpg' || type === 'png' || type === 'svg'){
+									mainImage = query[key].title.substring(5);		
+									break;				
+								}				
+							}			
+						}
+					}											
+				}
+
+			};
+
+			if ( !mainImage || mainImage == "" || mainImage == null ){
+				mainImage = "Disambig gray.svg";
+			}
+
+			imagesArr.push(mainImage);
+
+		})
+
+	}).then(imageURL => {
+		getimagesUrl(data);
+	})
+}
+
+
+let getimagesUrl = (data) => {
+
+	let urls = [];
+
+	for(let i = 0; i < data.query.search.length; i++){
+	
+		let imageName = imagesArr[i];
+		let urlConstructor = "https://en.wikipedia.org/w/api.php?action=query&format=json&titles=Image:"+imageName+"&prop=imageinfo&iiprop=url&origin=*";
+		urls.push(urlConstructor);
+		
+	}
+
+	Promise.all(urls.map(url =>
+	    fetch(url).then(resp => resp.json())
+	)).then(urlData => {
+
+		urlData.map((value, index) => {
+			results[index].imageURL = value.query.pages[Object.keys(value.query.pages)[0]].imageinfo[0].url;
+		});
+
+	}).then(getThumbs =>{
+		getThumb(data);
+	});
+
+}
+
+let getThumb = (data) => {	
+
+	let urls = [];
+
+	for(let i = 0; i < data.query.search.length; i++){
+	
+		let title = data.query.search[i].title;
+		let urlConstructor = "https://en.wikipedia.org/w/api.php?action=query&titles="+title+"&prop=pageimages&format=json&pithumbsize=300&origin=*";
+		urls.push(urlConstructor);
+	}
+
+	Promise.all(urls.map(url =>
+	    fetch(url).then(resp => resp.json())
+	)).then(thumbData => {
+
+		thumbData.map((value, index) => {
+
+			if(value.query.pages[results[index].pageid].thumbnail){
+        		results[index].imageThumb = value.query.pages[results[index].pageid].thumbnail.source;
+        	}else{
+        		results[index].imageThumb = '';
+        	}
 			
+		});
 
-			$("#results").append(""+
-				"<div class='col-3 mx-2 my-2'>"+
-				"<div class='card' style='width: 18rem;'>"+imageData+
-				"<div class='card-body'>"+
-				"<h5 class='card-title'>"+results[i].title+"</h5>"+
-				"<p class='card-text'>"+results[i].snippet+"</p>"+
-				"<a href='"+results[i].pageURL+"' target='_blank' class='btn btn-primary'>Read More</a>"+
-				"</div></div></div>");
+	}).then(populateAll =>{
+		populate();
+	});
+}
 
+
+let populate = () => {
+
+	document.getElementById("results").innerHTML = "";
+	document.getElementById("searchTerm").innerHTML = "<h3>Results for: "+searchTerm+"</h3>";
+
+
+	for(var i =0; i<results.length; i++){
+
+		var imageData = "";
+
+		if(results[i].imageThumb && results[i].imageThumb !== ""){
+			imageData = "<img class='card-img-top' src='"+results[i].imageThumb+"' alt='Card image cap'>";
+		}else if (results[i].imageURL !== "" ){
+			imageData = "<img class='card-img-top' src='"+results[i].imageURL+"' alt='Card image cap'>";
+		}else{
+			imageData = "<img class='card-img-top' src='https://upload.wikimedia.org/wikipedia/commons/d/de/Wikipedia_Logo_1.0.png' alt='Card image cap'>";
 		}
+
+		document.getElementById("results").innerHTML += ""+
+			"<div class='mx-2 my-2'>"+
+			"<div class='card align-self-stretch'>"+imageData+
+			"<div class='card-body'>"+
+			"<h5 class='card-title'>"+results[i].title+"</h5>"+
+			"<p class='card-text'>"+results[i].snippet+"</p>"+
+			"<a href='"+results[i].pageURL+"' target='_blank' class='btn btn-primary'>Read More</a>"+
+			"</div></div></div>";
 
 	}
 
-});
-
-
+}
